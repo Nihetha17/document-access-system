a2f597bf2e2ba4f233cf0a394e8a58fe51c76c0b
Nihetha17 <nihethanihe@gmail.com>
2026-03-12 09:55:01 +0530

    Added authMiddleware to routes

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
-const express=require("express")
-const router=express.Router()
-const controller=require("../controllers/mainController")
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/mainController")
 
 router.post("/create",authMiddleware, controller.create)
 router.get("/list",authMiddleware, controller.list)
