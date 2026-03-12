4b443c99acfea23f4cb6f6f048d0a0b6efc5a63d
Nihetha17 <nihethanihe@gmail.com>
2026-03-12 09:36:27 +0530

    Edited routes

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,8 @@ const express=require("express")
 const router=express.Router()
 const controller=require("../controllers/mainController")
 
-router.post("/create",controller.create)
-router.get("/list",controller.list)
-router.delete("/:id",controller.remove)
+router.post("/create",authMiddleware, controller.create)
+router.get("/list",authMiddleware, controller.list)
+router.delete("/:id",authMiddleware, controller.remove)
 
 module.exports=router
